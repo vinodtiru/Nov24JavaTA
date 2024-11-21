fa7cf1659c5bb3c2160e768a4025871a528ebca9
vinod <vinod.kumar@tekarch.com>
2024-11-21 09:47:08 +0530

    Removed Old Code

--- src/JavaOOPS/Banking/CurrentAccount.java
+++
@@ -1,8 +0,0 @@
-package JavaOOPS.Banking;
-
-public class CurrentAccount extends Account{
-    private Integer overdraftLimit;
-    private Integer transcationLimit;
-
-
-}
--- src/JavaOOPS/Banking/SavingAccount.java
+++
@@ -1,8 +0,0 @@
-package JavaOOPS.Banking;
-
-public class SavingAccount extends Account{
-    private Double interestRate;
-    private Double minimumBalance;
-    private Integer withdrawalLimit;
-
-}
