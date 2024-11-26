677cbd6b9dbcfda5ac1c9fa1f2281101d156f835
vinod <vinod.kumar@tekarch.com>
2024-11-26 08:51:57 +0530

    Added Logging

--- src/main/java/com/tekarch/customerms/Services/AccountServiceImpl.java
+++ src/main/java/com/tekarch/customerms/Services/AccountServiceImpl.java
@@ -3,12 +3,16 @@ package com.tekarch.customerms.Services;
 import com.tekarch.customerms.Models.Account;
 import com.tekarch.customerms.Repositories.AccountRepository;
 import com.tekarch.customerms.Services.Interfaces.AccountsService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class AccountServiceImpl implements AccountsService {
+    private static final Logger logger = LogManager.getLogger(AccountServiceImpl.class);
+
     private final AccountRepository accountRepository;
 
     public AccountServiceImpl(AccountRepository accountRepository) {
@@ -17,6 +21,12 @@ public class AccountServiceImpl implements AccountsService {
 
     @Override
     public Account addAccount(Account account) {
+        logger.info("New request to add Account"); //common
+        logger.trace("New request to add Account");
+        logger.error("New request to add Account with number  {} with balance {}. There was a error",
+                account.getAccountType(), account.getBalance()); //always
+        logger.debug("New request to add Account"); //sometimes
+        logger.warn("New request to add Account");
         return accountRepository.save(account);
     }
 
--- src/main/java/com/tekarch/customerms/Services/CustomerServiceImpl.java
+++ src/main/java/com/tekarch/customerms/Services/CustomerServiceImpl.java
@@ -3,6 +3,8 @@ package com.tekarch.customerms.Services;
 import com.tekarch.customerms.Models.Customer;
 import com.tekarch.customerms.Repositories.CustomerRepository;
 import com.tekarch.customerms.Services.Interfaces.CustomerService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,6 +12,7 @@ import java.util.List;
 @Service
 public class CustomerServiceImpl implements CustomerService {
     private final CustomerRepository customerRepository;
+    private static final Logger logger = LogManager.getLogger(CustomerServiceImpl.class);
 
     public CustomerServiceImpl(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
@@ -17,6 +20,8 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public Customer addCustomer(Customer customer) {
+        System.out.println("New request to add customer ");
+        logger.info("New request to add customer and i am logging it");
         return customerRepository.save(customer);
     }
 
