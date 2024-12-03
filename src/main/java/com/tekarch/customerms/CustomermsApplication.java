19496ddebde7694f5bf5ec80333a445601ab508a
vinod <vinod.kumar@tekarch.com>
2024-12-03 07:46:04 +0530

    Added Constructors to our model classes

--- src/main/java/com/tekarch/customerms/CustomermsApplication.java
+++ src/main/java/com/tekarch/customerms/CustomermsApplication.java
@@ -1,5 +1,7 @@
 package com.tekarch.customerms;
 
+import com.tekarch.customerms.Models.Account;
+import com.tekarch.customerms.Models.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,31 @@ public class CustomermsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomermsApplication.class, args);
+
+		Account account = new Account();
+		account.setAccountNumber(1234L);
+		account.setBalance(2000.0);
+		Long myAcct = account.getAccountNumber();
+
+
+		Student student = new Student();
+		student.setName("Ramila");
+		student.setId(5L);
+		student.setAge(20);
+		student.setAddress("Bangalore India");
+		student.setAvgScore(45);
+		student.setRollNumber("12345ABCD");
+
+		Student student2 = new Student(5L,"Vinod","Bangalore India", 20, 45,
+				"12345ABCD");
+//		student2.setName("Vinod");
+//		student2.setId(5L);
+//		student2.setAge(20);
+//		student2.setAddress("Bangalore India");
+//		student2.setAvgScore(45);
+//		student2.setRollNumber("12345ABCD");
+
+
 	}
 
 }
--- src/main/java/com/tekarch/customerms/Models/Student.java
+++ src/main/java/com/tekarch/customerms/Models/Student.java
@@ -7,6 +7,20 @@ import lombok.Data;
 @Table
 @Data
 public class Student {
+
+
+    public Student() {
+    }
+
+    public Student(Long id, String name, String address, Integer age, double avgScore, String rollNumber) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.age = age;
+        this.avgScore = avgScore;
+        this.rollNumber = rollNumber;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
