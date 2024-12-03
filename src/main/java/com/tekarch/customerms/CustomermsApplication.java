629c2925ff67af3b4003a955880e02bdd7a04861
vinod <vinod.kumar@tekarch.com>
2024-12-03 07:53:14 +0530

    Removed unwanted code

--- src/main/java/com/tekarch/customerms/CustomermsApplication.java
+++ src/main/java/com/tekarch/customerms/CustomermsApplication.java
@@ -11,30 +11,6 @@ public class CustomermsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CustomermsApplication.class, args);
 
-		Account account = new Account();
-		account.setAccountNumber(1234L);
-		account.setBalance(2000.0);
-		Long myAcct = account.getAccountNumber();
-
-
-		Student student = new Student();
-		student.setName("Ramila");
-		student.setId(5L);
-		student.setAge(20);
-		student.setAddress("Bangalore India");
-		student.setAvgScore(45);
-		student.setRollNumber("12345ABCD");
-		student.setEmail("ramila@tekarch.com");
-
-		Student student2 = new Student(5L,"Vinod","Bangalore India", 20, 45,
-				"12345ABCD", "vinod@tekarch.com", "vinod2@gmail.com");
-//		student2.setName("Vinod");
-//		student2.setId(5L);
-//		student2.setAge(20);
-//		student2.setAddress("Bangalore India");
-//		student2.setAvgScore(45);
-//		student2.setRollNumber("12345ABCD");
-
 
 	}
 
